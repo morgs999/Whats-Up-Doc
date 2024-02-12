ac31f42031de9ab97007f1c2b611cf1ac11aea9b
Morgan <morgs99@gmail.com>
2024-02-12 13:57:40 -0500

    got dropdown for procedures working

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from "react-slick";
 import Calendar from '../components/Calendar';
 import ProceduresList from '../components/ProceduresList';
@@ -13,6 +13,7 @@ const Appointment = () => {
 
   const { loading, data } = useQuery(QUERY_ALL_PROCEDURE);
   const procedures = data?.procedures || [];
+  // console.log(procedures, "hello morgan");
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -37,13 +38,13 @@ const Appointment = () => {
     (ADD_APPOINTMENT);
 
   // const [addProcedure, { procerror }] = useMutation(ADD_PROCEDURE);
-  
+
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedTime, setSelectedTime] = useState(null);
 
   const handleDateSelect = (date) => {
     setSelectedDate(date);
-    setSelectedTime(null); 
+    // setSelectedTime(null); 
   };
 
   const handleTimeSelect = (time) => {
@@ -65,20 +66,20 @@ const Appointment = () => {
       {selectedTime && (
         <p>Selected appointment time: {selectedTime}</p>
       )}
-      
-      <ProceduresList procedures={procedures} />
-      <div className="m-5 col-6 justify-center">
+
+
+      {/* <div className="m-5 col-6 justify-center">
         <Slider >
           {procedures.map((procedure) => (
             <div
               className="h-[400px] text-black rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] mb-2 cursor-pointer"
               key={procedure.procedureName}
-            >aaaa
+            >
               <button onClick={handleFormSubmit} className='border rounded-xl w-full my-5 mx-2 py-2 px-2 bg-indigo-600 hover:bg-indigo-500 text-white' value={procedure.procedureName} style={{ cursor: 'pointer' }}>{`${procedure.procedureName}`}</button>
             </div>
           ))}
         </Slider>
-      </div>
+      </div> */}
 
     </div>
   );
